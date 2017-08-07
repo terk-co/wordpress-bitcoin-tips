9faeef215e10cf218870f4bf82316d4960a7a3ef
Terk <30804421+terk-co@users.noreply.github.com>
2017-08-07 21:01:43 +0200

    Updated plugin to work with Blockchain.info API v2. Using BIP32/44 xPub addresses now.

--- src/admin.php
+++ src/admin.php
@@ -35,18 +35,35 @@ class BitcointipsSettings {
     screen_icon();
   	echo '<h2>Bitcoin Tips</h2>';
   	?>
-      <p>
+  	<div style="width: 800px;">
+      <p><strong>
         Bitcoin Tips plugin allows you collecting bitcoin tips from your readers. Every post gets its unique bitcoin address
         for tips. This allows for detailed stats of tips per post, so you know which texts your readers appreciate the most.
-        All tips are immediately forwarded to your defined bitcoin address, so  tips from all posts
-        are eventually sent to a single address. The plugin uses Blockchain.info API to collect your tips (this adds some
-        level of security, as even if your Wordpress is hacked, your received tips are secure as there are no bitcoin
-        private keys stored in your Wordpress).
+      </strong>
       </p>
+      <p>
+        Only you controls the tips during the entire process. You need to have a BIP32/44 deterministic wallet in order to
+        use this plugin (e.g. Blockchain.info or Trezor wallets) and you configure your wallet xPub in the plugin.
+        The xPub allows generating public keys for your wallet without knowing private keys. This is how the plugin generates
+        unique addresses for blog posts. Tips are sent directly to your wallet.
+      </p>
+      <p>
+        A minor issue with BIP 44 wallets is that most of them scan only up to 20 unused addresses. If you have more than 20
+        posts without any tips and then you receive a tip for the 21st or later, your wallet might not display it to you.
+        The tip is still there and it's yours, but your wallet will not know about it. Please be aware of this.
+        If you don't get many tips, it might be useful to generate new xPub keys when you approach to 20 not-tipped addressed.
+      </p>
+      <p>
+        The plugin uses Blockchain.info API and you need to
+        <a href="https://api.blockchain.info/customer/signup" target="_blank">apply for an API key at their website</a>.
+        I might create an independent plugin in the feature (not requiring Blockchain.info API), but there needs to be enough
+        interest in that as it requires some more development time.
+      </p
       <p>
     	 Both plugin and Blockchain.info API are free to use, though <strong>if you like this plugin, donations are welcome in the form of tips on the
   	 <a href="<?php echo BITCOINTIPS_HOME_URL; ?>" target="_blank">plugin home page</a></strong>.
   	 </p>
+    </div>
   	<?php
   	echo '<form method="post" action="options.php">';
   	settings_fields('bitcointips');
@@ -71,7 +88,9 @@ class BitcointipsSettings {
   
   public function register_settings() {
     add_settings_section('bitcointips', 'Your Bitcoin Tips Settings', array($this, 'show_settings'), 'bitcointips');
-    $this->add_field('address', 'Destination address to forward tips');
+    $this->add_field('xpub', 'Your wallet xPub');
+    $this->add_field('xpubgap', 'xPub gap allowed<br />(Advanced)');
+    $this->add_field('biapikey', 'Blockchain.info API key');
     $this->add_field('label', 'Tip box label');
     $this->add_field('text', 'Tip box text');
     $this->add_field('stats', 'Show public stats');
@@ -102,6 +121,35 @@ class BitcointipsSettings {
         '<strong>Important:</strong> If you change this address to a new one, only tips for posts created after the change ',
         'will be sent to the new address and tips for previous posts will still be sent the the previous address. ',
         'This will be changed in one of next versions.',
+      '</p>'
+    ;
+  }
+  
+  public function show_field_biapikey() {
+    echo 
+      '<input name="bitcointips_biapikey" type="text" class="bitcointips-width" value="' . get_option('bitcointips_biapikey') . '"/><br />',
+      '<p class="description bitcointips-width">',
+        'Your Blockchain.info API key. <a href="https://api.blockchain.info/customer/signup" target="_blank">You can request it at their website</a>',
+      '</p>'
+    ;
+  }
+  
+  public function show_field_xpub() {
+    echo 
+      '<input name="bitcointips_xpub" type="text" class="bitcointips-width" value="' . get_option('bitcointips_xpub') . '"/><br />',
+      '<p class="description bitcointips-width">',
+        '<strong>Important:</strong> If you change this address to a new one, only tips for posts created after the change ',
+        'will be sent to the new xPub account and tips for previous posts will still be sent the the previous address. ',
+      '</p>'
+    ;
+  }
+
+  public function show_field_xpubgap() {
+    $value = ((integer) get_option('bitcointips_xpubgap')) >= 20 ? (integer)get_option('bitcointips_xpubgap') : 20; 
+    echo 
+      '<input name="bitcointips_xpubgap" type="text" value="' . $value . '" size=4/><br />',
+      '<p class="description bitcointips-width">',
+        'New addresses won\'t be generated after you reach this number of unused addresses. Increase this number only if you understand BIP44 gap and how your wallet handles it',
       '</p>'
     ;
   }
